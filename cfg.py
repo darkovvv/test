e7cdb3329efb9c86d431131110a6513f66377b0a
darkovvv <136109733+darkovvv@users.noreply.github.com>
2023-06-09 22:47:05 +0300

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,10 @@
+token = "vk1.a.cTR60I8M8X1l67zPg3vl_GpB2XhDFksGmx5Vyx2JqCRgIP22p793zpWSnC66Fr1NejbhqOdDD7FxCj5CztyoMfs1p7NC9brMz0ZHI70joomuEboLmf19E1Jq3lcltGD8sbitoC3jGPkxfnOlL0sIvHH8A9vvTdsZc53V3BfpbouazYZxG-6dde1851KXZPnk0tWrpM7AwL27iwCdnXrEbQ"
+# токен аккаунта
+chance = 2
+# шанс ответа
+acc_id = 11971510
+# айди пользователя
+user_ignore = [236414911]
+# игнор пользователей по айди
+chat_ignore = [29, 30, 32]
+# лист айди чатов с игнором
